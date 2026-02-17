7624c3d4264038a25faa1a51e0ffaf1fe34f0b6f
Burhon Abdullaev <abdullaev_b@turonbank.uz>
2026-02-17 18:03:31 +0500

    add if, else

--- main.rs
+++ main.rs
@@ -8,20 +8,32 @@
 
 //   Add number 
 
-fn main() {
-    let add = 5 + 3;
-    let sub = 10 - 4;
-    let mul = 6 * 2;
-    let div = 12 / 3;
-    let rem = 10 % 3;
+// fn main() {
+//     let add = 5 + 3;
+//     let sub = 10 - 4;
+//     let mul = 6 * 2;
+//     let div = 12 / 3;
+//     let rem = 10 % 3;
   
-    println!("Add: {}", add);
-    println!("Sub: {}", sub);
-    println!("Mul: {}", mul);
-    println!("Div: {}", div);
-    println!("Rem: {}", rem);
-  }
+//     println!("Add: {}", add);
+//     println!("Sub: {}", sub);
+//     println!("Mul: {}", mul);
+//     println!("Div: {}", div);
+//     println!("Rem: {}", rem);
+//   }
 
+
+//  Rust if , else 
+fn main() {
+    let a = 5;
+    let b = 10;
+
+    if a > b {
+        println!("a  is greater than b.");
+    }   else {
+        println!("b is greater than a .");
+    }
+}
 
 
 //fn main() {
